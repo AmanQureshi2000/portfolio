edb723e4d1ca2bbed7f778e618d27810324ecd89
AmanQureshi <aman.qureshi@indiamart.com>
2025-07-21 20:27:56 +0530

    new

---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Navbar() {
+  return (
+    <nav>
+      <ul>
+        <li><a href="#hero">Home</a></li>
+        <li><a href="#skills">Skills</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#projects">Projects</a></li>
+        <li><a href="#education">Education</a></li>
+        <li><a href="#achievements">Achievements</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
